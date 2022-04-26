74687f67a57982d6e170fa8ce253d23cf58a7cbd
MagyDrop17 <dvalenciamarin@gmail.com>
2022-04-26 13:24:49 +0200

    last

--- resources/views/casals/afegir.blade.php
+++ resources/views/casals/afegir.blade.php
@@ -7,21 +7,33 @@
         <div class="form-group">
             <label for="nom">Nom</label>
             <input type="text" class="form-control" name="nom" placeholder="Nom" required/>
+            @error('nom')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="data_inici">Data Inici</label>
             <input type="date" class="form-control" name="data_inici" placeholder="date" required/>
+            @error('data_inici')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="data_final">Data Final</label>
             <input type="date" class="form-control" name="data_final" placeholder="date" required/>
+            @error('data_final')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="n_places">Num Places</label>
             <input type="integer" class="form-control" name="n_places" placeholder="50" required/>
+            @error('n_places')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
@@ -31,6 +43,9 @@
                     <option value="{{ $ciu->id }}">{{ $ciu->nom }}</option>
                 @endforeach
             </select>
+            @error('ciutat_id')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
             <input type="submit" class="btn btn-primary" role="button" value="Afegeix"></input>
--- resources/views/casals/editar.blade.php
+++ resources/views/casals/editar.blade.php
@@ -11,21 +11,33 @@
         <div class="form-group">
             <label for="nom">Nom</label>
             <input type="text" class="form-control" name="nom" placeholder="Nom" value="{{$casal->nom}}" required/>
+            @error('nom')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="data_inici">Data Inici</label>
             <input type="date" class="form-control" name="data_inici" placeholder="date" value="{{$casal->data_inici}}" required/>
+            @error('data_inici')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="data_final">Data Final</label>
             <input type="date" class="form-control" name="data_final" placeholder="date" value="{{$casal->data_final}}" required/>
+            @error('data_final')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="n_places">Num Places</label>
             <input type="integer" class="form-control" name="n_places" placeholder="50" value="{{$casal->n_places}}" required/>
+            @error('n_places')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="form-group">
@@ -33,13 +45,16 @@
             <select class="form-control" name="ciutat_id">
                 @foreach ($ciutat as $ciu)
 
-                    @if($ciu->id == $casal->ciutat_id)
+                    @if($ciu->id == $casal->ciutat->id)
                         <option value="{{ $ciu->id }}" selected>{{ $ciu->nom }}</option>
                     @else
                         <option value="{{ $ciu->id }}">{{ $ciu->nom }}</option>
                     @endif
                 @endforeach
             </select>
+            @error('ciutat_id')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
             <input type="submit" class="btn btn-primary" role="button" value="Afegeix"></input>
